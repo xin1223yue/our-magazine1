41540d6e7cebe26abcd994d4f0a8a57b69a7ac90
xin1223yue <166989576+xin1223yue@users.noreply.github.com>
2026-07-09 19:10:26 +0800

    Create [key].js

---
+++ functions/api/photo/[key].js
@@ -0,0 +1,14 @@
+export async function onRequestGet({ env, params }) {
+  const object = await env.MY_BUCKET.get(params.key);
+
+  if (!object) {
+    return new Response("Not found", { status: 404 });
+  }
+
+  const headers = new Headers();
+  object.writeHttpMetadata(headers);
+  headers.set("etag", object.httpEtag);
+  headers.set("cache-control", "public, max-age=31536000, immutable");
+
+  return new Response(object.body, { headers });
+}
